9a022c4fb802032481e6ffeecc2fbcaee80a123c
Jack Kelly <jack@openclimatefix.org>
2023-09-20 18:16:44 +0100

    initial commit. Just fleshing out a simple example

---
+++ perfcapture/examples/read_numpy_file.py
@@ -0,0 +1,36 @@
+import os
+import tempfile
+
+import numpy as np
+
+
+class ReadNumpyFile:  # TODO: Should inherit from WorkLoad ABC.
+    def before_each_run(self):
+        """Create simple numpy file in a temp directory."""
+        # Generate an array of random numbers
+        rng = np.random.default_rng()
+        DTYPE = np.uint8
+        low, high = np.iinfo(DTYPE).min, np.iinfo(DTYPE).max
+        array = rng.integers(
+            low=low, 
+            high=high, 
+            size=(100, 100, 100, 100),
+            dtype=DTYPE,
+            )
+        print("Created array", flush=True)
+        
+        # Save array to temporary file
+        fh, self.filename = tempfile.mkstemp()
+        os.close(fh)
+        with open(self.filename, mode="wb") as fh:
+            np.save(fh, array)
+    
+    def run(self):
+        """Load numpy file into RAM."""
+        for _ in range(100):
+            np.load(self.filename)
+    
+    def after_each_run(self):
+        """Remove numpy file from temp directory."""
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
---
+++ setup.py
@@ -0,0 +1,22 @@
+""" Usual setup file for package """
+# read the contents of your README file
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="perfcapture",
+    version="0.0.1",
+    license="MIT",
+    description="Capture the performance of a computer system whilst running a set of benchmark workloads.",
+    author="Jack Kelly",
+    author_email="info@openclimatefix.org",
+    company="Open Climate Fix Ltd",
+    install_requires=[],  # TODO
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+)
