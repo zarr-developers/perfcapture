da234bb03fefc11813f4f294eee6b56e64c80960
Jack Kelly <jack@openclimatefix.org>
2023-09-29 12:51:46 +0100

    Configure Ruff to check docstrings

--- src/perfcapture/utils.py
+++ src/perfcapture/utils.py
@@ -1,8 +1,10 @@
+"""Simple utility functions."""
+
 import pathlib
 
 
 def path_not_empty(path: pathlib.Path) -> bool:
-    "Returns True if `path` is not empty."
+    """Returns True if `path` is not empty."""
     # If `path` contains just a single entry then return True.
     # To save time, don't bother iterating past the first entry.
     for _ in path.iterdir():
