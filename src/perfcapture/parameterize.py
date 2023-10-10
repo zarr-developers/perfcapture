22191786a95c8a1040f2e600a9d97bd3e145c1e8
Jack Kelly <jack@openclimatefix.org>
2023-10-10 12:21:48 +0100

    parametrize no longer clobbers kwargs.
    And refactor to convert singular argvalues to tuples of length 1.

--- src/perfcapture/parameterize.py
+++ src/perfcapture/parameterize.py
@@ -16,26 +16,37 @@ def parameterize(
     >>> f()
     1 2
     3 4
+    >>> f(foo=0)
+    0 2
+    0 4
+    >>> @parameterize("baz", [1, 2])
+    ... def g(baz):
+    ...     print(baz)
+    >>> g()
+    1
+    2
     
     See pytest's docs on the parameterize mark:
     https://docs.pytest.org/en/7.3.x/how-to/parametrize.html
     """
     argnames = argnames.split(',')
+    
+    # Convert a singular argvalue to a tuple of length 1:
+    if len(argnames) == 1:
+        for i, argvalue in enumerate(argvalues):
+            argvalues[i] = (argvalue,)
 
     # Sanity check
-    if len(argnames) > 1:
-        for argvalue in argvalues:
-            assert len(argvalue) == len(argnames)
+    for argvalue in argvalues:
+        assert len(argvalue) == len(argnames)
 
     def decorator_parameterize(func: Callable) -> Callable:
         @functools.wraps(func)
         def wrapper_parameterize(*args, **kwargs) -> None:
             for argvalue in argvalues:
-                if len(argnames) == 1:
-                    kwargs[argnames[0]] = argvalue
-                else:
-                    for i, argname in enumerate(argnames):
-                        kwargs[argname] = argvalue[i]
-                func(*args, **kwargs)
+                modified_kwargs = kwargs.copy()
+                for i, argname in enumerate(argnames):
+                    modified_kwargs.setdefault(argname, argvalue[i])
+                func(*args, **modified_kwargs)
         return wrapper_parameterize
     return decorator_parameterize
