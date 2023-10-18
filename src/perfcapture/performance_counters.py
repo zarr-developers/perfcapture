6b947426bd3c2d92447a37ab550f958217cf5fe0
Jack Kelly <jack@openclimatefix.org>
2023-10-18 12:03:41 +0100

    Fix rename (again!)

--- src/perfcapture/performance_counters.py
+++ src/perfcapture/performance_counters.py
@@ -137,7 +137,7 @@ class DiskIO(_PerfCounterABC):
             del count_diff[f"{direction}_bytes"]
 
         # Append time unit:
-        count_diff.rename(columns={
+        count_diff = count_diff.rename({
             "read_time": "read_time_ms",
             "write_time": "write_time_ms",
             "busy_time": "busy_time_ms",
